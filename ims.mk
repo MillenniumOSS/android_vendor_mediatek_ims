29415fe954a6f2124109b46e877d2be2818bef81
Mashopy <eliasgheeraert@gmail.com>
2025-06-15 10:53:15 +0530

    mediatek: ims: Track com.mediatek.ims.config.xml sysconfig
    This add the IMS package into the whitelist for ignoring location settings (emergency purposes).
    
    Change-Id: If2e59ed117bef992d55eb429d0a0f3814a20c6b9

--- ims.mk
+++ ims.mk
@@ -7,7 +7,8 @@ IMS_VNDR_PATH := vendor/mediatek/ims
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    $(IMS_VNDR_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
+    $(IMS_VNDR_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-com.mediatek.ims.xml \
+    $(IMS_VNDR_PATH)/configs/sysconfig/com.mediatek.ims.config.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/com.mediatek.ims.config.xml
 
 # libui_shim
 PRODUCT_PACKAGE += \
