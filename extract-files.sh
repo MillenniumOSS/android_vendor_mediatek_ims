326fce03a07925c5bbb0aca2bcc84ed62c044dcc
bengris32 <bengris32@protonmail.ch>
2026-02-23 10:44:31 +0800

    ims :Add shim for MediaTek AudioTrack constructor into libsink-mtk
    Change-Id: Icc7a1f9c748578f5b58499e7a76213673c4bcf54
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- extract-files.sh
+++ extract-files.sh
@@ -52,6 +52,14 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        system_ext/lib64/libsink-mtk.so)
+            "${PATCHELF}" --add-needed libaudioclient_shim.so "${2}"
+            ;;
+    esac
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
